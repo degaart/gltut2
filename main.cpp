4e462858a4c3db29a5d86918a3203a599880b527
degaart <degaart@users.noreply.github.com>
2017-11-28 18:51:29 +0300

    Draw textured rectangle

--- main.cpp
+++ main.cpp
@@ -4,11 +4,14 @@
 #include <cstdio>
 #include <cmath>
 #include <memory>
+#include "stb_image.h"
 #include "shader.h"
+#include "exception.h"
 
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
 
+#define BUFFER_OBJECT(i) ((void*)(i))
 
 
 // Callback called when the window is resized
@@ -28,38 +31,46 @@ static void processInput(GLFWwindow *window)
 struct gl_context {
     std::shared_ptr<Shader> program;
     unsigned int VAO;
+    unsigned int texture;
 };
 
 static gl_context prepare_context()
 {
-    // data for 2 triangles next to each other
     float vertices[] = {
-        // positions         // colors
-        0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
-        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
-        0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
+        // positions          // colors           // texture coords
+         0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+         0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
     };
+    unsigned int indices[] = {  // note that we start from 0!
+        0, 1, 3,                // first triangle
+        1, 2, 3                 // second triangle
+    }; 
 
-    // Create 2 VAO for the two triangles
     unsigned int VAO; 
     glGenVertexArrays(1, &VAO);
 
-    // Create 2 VBO for the 2 datasets
     unsigned int VBO;
     glGenBuffers(1, &VBO);
 
-    // Triangle 0, buffer
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW); 
 
     // Vertex position attributes
     glVertexAttribPointer(0,                    /* index (in the vertex shader source, position=0) */
                           3,                    /* size of vertex attribute */ 
                           GL_FLOAT,             /* type */
                           GL_FALSE,             /* do not normalize */
-                          6 * sizeof(float),    /* space between consecutive vertex attribute sets */
-                          (void*)0);            /* starting position */
+                          8 * sizeof(float),    /* space between consecutive vertex attribute sets */
+                          BUFFER_OBJECT(0));    /* starting position */
     glEnableVertexAttribArray(0);               /* enable attribute at location 0 */
 
     // Vertex color attribute
@@ -67,17 +78,55 @@ static gl_context prepare_context()
                           3,
                           GL_FLOAT,
                           GL_FALSE,
-                          6 * sizeof(float),
-                          (void*)(3 * sizeof(float)));
+                          8 * sizeof(float),
+                          BUFFER_OBJECT(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
+    // Vertex texture coords attribute
+    glVertexAttribPointer(2,
+                          2,
+                          GL_FLOAT,
+                          GL_FALSE,
+                          8 * sizeof(float),
+                          BUFFER_OBJECT(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    // Texture stuffs
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); // x-coord
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT); // y-coord
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);  // scaling downward
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);   // scaling upward
+
+    int width, height, nrChannels;
+    unsigned char *data = stbi_load("res/container.jpg", &width, &height, &nrChannels, 0); 
+    if(!data) {
+        throw Exception("Failed to load texture");
+    }
+
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexImage2D(GL_TEXTURE_2D,
+                 0,                 /* mipmap level */
+                 GL_RGB,            /* format to store into */
+                 width, height,
+                 0,                 /* unused legacy stuff */
+                 GL_RGB,            /* input format */
+                 GL_UNSIGNED_BYTE,  /* input datatype */
+                 data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    stbi_image_free(data);
+
     // Create shaders
-    auto shader = std::make_shared<Shader>("res/shader3.vs", "res/shader3.fs");
+    auto shader = std::make_shared<Shader>("res/shader4.vs", 
+                                           "res/shader4.fs");
 
     // Return results
     gl_context result;
     result.program = shader;
     result.VAO = VAO;
+    result.texture = texture;
     return result;
 }
 
@@ -87,7 +136,9 @@ static void draw(gl_context* context, float ticks)
     context->program->setFloat("off", sin(ticks) / 2.0f);
 
     glBindVertexArray(context->VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glBindTexture(GL_TEXTURE_2D, context->texture);
+    //glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
     glBindVertexArray(0);
     glUseProgram(0);
--- res/shader.vs
+++ res/shader.vs
@@ -4,8 +4,10 @@
 
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
+layout (location = 2) in vec2 aTexCoord;
 
 out vec3 ourColor;
+out vec2 TexCoord;
 
 uniform float off;
 
@@ -13,6 +15,7 @@ void main()
 {
     gl_Position = vec4(aPos.x + off, aPos.y, aPos.z, 1.0);
     ourColor = aColor;
+    TexCoord = aTexCoord;
 }
 
 
